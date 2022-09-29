916568b23e2aff16392c2844d864db1a60d5f90c
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2022-09-30 00:13:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <h1 data-ns-test="project-name">portfilo website</h1>
-      <p data-ns-test="project-name">This is a working portfilo website</p>
+      <div data-ns-test="project-name">This is a working portfilo website</div>
     </>
   )
 }
